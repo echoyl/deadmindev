4eee8e3d25ec252b0a020f84b6258397c7c89d5d
echoyl <yliang_1987@126.com>
2024-05-06 14:06:58 +0800

    0.8.4

--- config/proxy.ts
+++ config/proxy.ts
@@ -12,15 +12,7 @@ export default {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/sadmin/': {
       // 要代理的地址
-      //target: 'http://wxapp.la/ganchangzichan/public',
-      //target: 'http://wangzhan.la/jiangxichuangtou/public',
-      //target: 'http://wx.la/huayuan/public',
-      //target: 'http://wangzhan.la/pro/public',
-      //target: 'http://wxapp.la/ganchangshangcheng/public',
       target: 'http://echoyl.la',
-      //target: 'http://wangzhan.la/hzjt6666/public',
-      //target: 'http://wxapp.la/anjiwuliu/public',
-      //target: 'http://wxapp.la/lvyouziyuanpucha/public',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
