df4d733ee1a459ffc160e282d910726db2fa1c7c
echoyl <yliang_1987@126.com>
2024-09-20 16:52:47 +0800

    字段增加mdEditor选项

--- src/components/Sadmin/dev/vars/model/fieldColumns.tsx
+++ src/components/Sadmin/dev/vars/model/fieldColumns.tsx
@@ -167,6 +167,7 @@ const formType = [
   { label: '弹层选择 - modalSelect', value: 'modalSelect' },
   { label: '拾色器 - colorPicker', value: 'colorPicker' },
   { label: '图标选择器 - iconSelect', value: 'iconSelect' },
+  { label: 'markdown编辑器 - mdEditor', value: 'mdEditor' },
 ];
 const fieldColumns: saFormColumnsType = [
   {
