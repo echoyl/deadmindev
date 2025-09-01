df7fdd3add26e5c969740c6da01f18655c777d0a
echoyl <inandan@126.com>
2025-09-01 16:14:20 +0800

    fix:后台用户角色权限选择组件，删除之前的文件

--- src/components/Sadmin/perm/group.tsx
+++ src/components/Sadmin/perm/group.tsx
@@ -3,81 +3,7 @@ import { TreeSelect } from 'antd';
 import { DataNode } from 'antd/es/tree';
 import React, { useContext, useEffect, useState } from 'react';
 import { SaContext } from '../posts/table';
-import PermPanel from './panel';
-import { t, tplComplie } from '../helpers';
-const PermGroup2: React.FC<{
-  perms?: Array<any>;
-  onChange?: (value: any) => void;
-  roleList?: string[] | boolean;
-  roleid?: number;
-  value?: any;
-}> = (props) => {
-  const { onChange, value, roleid } = props;
-  const [values, setValues] = useState({});
-  //const initValues = value ? value.split(',') : [];
-  //console.log('role value', value);
-  const [initValues, setInitValues] = useState([]);
-  const [perms, setPerms] = useState();
-  const [roleList, setRoleList] = useState();
-  const [rolePerms, setRolePerms] = useState();
-  //通过请求获取后台所有的权限及角色的权限
-  const getData = async (rid) => {
-    const { data } = await request.get('perm/role/perms');
-    setPerms(data.perms);
-    setRolePerms(data.role_perms);
-    //console.log('get data roleid is', rid);
-    if (rid) {
-      //console.log('has init roleid', rid, data.role_perms[rid]);
-      setRoleList(data.role_perms[rid]);
-    }
-  };
-  const { formRef } = useContext(SaContext);
-
-  useEffect(() => {
-    if (formRef.current) {
-      const _values = formRef?.current?.getFieldsValue();
-      if (_values.perms2) {
-        //初始化用户的权限 这里将纯数字的数据删除 本来就没用，会导致选择bug
-        //const ivalue = _values.perms2.split(',').filter((v) => !/^\d+$/.test(v));
-        const ivalue = _values.perms2.split(',');
-        setInitValues(ivalue);
-        //console.log('setInitValues here', ivalue);
-      }
-      getData(_values.roleid);
-      //console.log('formRef is ready', formRef?.current?.getFieldsValue());
-    }
-  }, [formRef]);
-
-  useEffect(() => {
-    if (roleid && rolePerms?.[roleid]) {
-      setRoleList(rolePerms?.[roleid]);
-      console.log('roleid is real change', rolePerms?.[roleid]);
-      formRef.current?.setFieldValue('perms2', rolePerms?.[roleid].join(','));
-    }
-  }, [roleid]);
-
-  return perms?.map((perm) => {
-    return (
-      <PermPanel
-        key={perm.value}
-        perm={perm}
-        userPerms={initValues}
-        roleList={roleList}
-        onChangeToParent={(state, _values) => {
-          //console.log(state, values);
-          values[perm.value] = _values;
-          setValues({ ...values });
-          //将数据解构
-          let _v: string[] = [];
-          for (var i in values) {
-            _v = [..._v, ...values[i]];
-          }
-          onChange?.([..._v].join(','));
-        }}
-      />
-    );
-  });
-};
+import { loopMenuLocale } from '../helpers';
 
 const PermGroup: React.FC<{
   onChange?: (value: any) => void;
@@ -95,7 +21,7 @@ const PermGroup: React.FC<{
     url = 'perm/role/perms',
   } = props;
   const [perms, setPerms] = useState<DataNode[]>();
-  const [value, setValue] = useState([]);
+  const [value, setValue] = useState<string[]>([]);
   const { formRef } = useContext(SaContext);
   const [init, setInit] = useState(false);
 
@@ -104,7 +30,7 @@ const PermGroup: React.FC<{
     if (code) {
       return;
     }
-    setPerms(data.perms);
+    setPerms(loopMenuLocale(data.perms || [], { children: 'options' }));
     //console.log('get data ivalue is', ivalue, init);
     if (!init) {
       setInit(true);
@@ -140,7 +66,7 @@ const PermGroup: React.FC<{
   const tProps = {
     treeData: perms,
     value,
-    fieldNames: { title: 'label', key: 'value', children: 'options' },
+    fieldNames: { label: 'label', value: 'value', children: 'options' },
     onChange,
     treeCheckable: true,
     //showCheckedStrategy: SHOW_PARENT,
@@ -155,10 +81,10 @@ const PermGroup: React.FC<{
   return (
     <TreeSelect
       {...tProps}
-      treeTitleRender={(item) => {
-        //console.log('item', item);
-        return item.label ? tplComplie(item.label) : item.label;
-      }}
+      // treeTitleRender={(item) => {
+      //   //console.log('item', item);
+      //   return item.label ? tplComplie(item.label) : item.label;
+      // }}
     />
   );
 };
