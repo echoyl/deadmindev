4abfde305b029433c52bbc64c9c6783a8916fd66
echoyl <inandan@126.com>
2026-08-12 15:47:48 +0800

    feat:footer优化

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,44 +1,34 @@
-import { DefaultFooter } from '@ant-design/pro-components';
 import { useModel } from '@umijs/max';
-import { theme } from 'antd';
+import { createStyles } from 'antd-style';
+import React from 'react';
+
+const useStyles = createStyles(({ token, css }) => ({
+  footer: css`
+    padding: 0 24px 16px;
+    text-align: center;
+    color: ${token.colorTextDescription};
+    font-size: ${token.fontSizeSM}px;
+    line-height: ${token.lineHeight};
+    background: transparent;
+  `,
+  copyright: css`
+    height: 22px;
+    line-height: 22px;
+  `,
+}));
 
 const Footer: React.FC = () => {
-  const currentYear = new Date().getFullYear();
+  const { styles } = useStyles();
+  const year = new Date().getFullYear();
   const { initialState } = useModel('@@initialState');
   const techContent = initialState?.settings?.adminSetting?.tech;
-  const tech = techContent ? `${currentYear} ${techContent}` : '';
-  const { token } = theme.useToken();
-  return !tech ? null : (
-    <>
-      <DefaultFooter
-        style={{
-          background: 'none',
-          color: token.colorTextBase,
-        }}
-        copyright={<span dangerouslySetInnerHTML={{ __html: tech }}></span>}
-        //copyright={<a href="#">test</a>}
-        // links={[
-        //   {
-        //     key: 'Ant Design Pro',
-        //     title: 'Ant Design Pro',
-        //     href: 'https://pro.ant.design',
-        //     blankTarget: true,
-        //   },
-        //   {
-        //     key: 'github',
-        //     title: <GithubOutlined />,
-        //     href: 'https://github.com/ant-design/ant-design-pro',
-        //     blankTarget: true,
-        //   },
-        //   {
-        //     key: 'Ant Design',
-        //     title: 'Ant Design',
-        //     href: 'https://ant.design',
-        //     blankTarget: true,
-        //   },
-        // ]}
-      />
-    </>
+
+  return (
+    <div className={styles.footer}>
+      <div className={styles.copyright}>
+        <span dangerouslySetInnerHTML={{ __html: techContent }}></span> &copy; {year}
+      </div>
+    </div>
   );
 };
 
