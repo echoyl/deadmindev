e5073041c064add414b74f2ae17a70d1c95892cd
echoyl <inandan@126.com>
2026-08-24 11:25:20 +0800

    fix:面板card显示优化

--- src/components/Sadmin/dev/panel/items/card.tsx
+++ src/components/Sadmin/dev/panel/items/card.tsx
@@ -1,13 +1,18 @@
 import { isObj } from '@/components/Sadmin/checkers';
 import { Statistic, StatisticCard } from '@ant-design/pro-components';
 import { Link } from '@umijs/max';
-import { Divider, Progress, Space } from 'antd';
+import { Progress, Space } from 'antd';
 import PanelItemChart from './chart';
 
-const PanelItemCard = (props) => {
-  const { title, data, config = {}, height, label } = props;
-  const { open } = config;
+interface PanelItemCardProps {
+  title?: string;
+  data?: Record<string, any>;
+  config?: Record<string, any>;
+  height?: number;
+  label?: string;
+}
 
+const PanelItemCard = (props: PanelItemCardProps) => {
   const getDescription = (config: Record<string, any>, data: Record<string, any>) => {
     const href = config?.href ? config.href : data?.href ? data.href : null;
     if (href) {
@@ -22,11 +27,10 @@ const PanelItemCard = (props) => {
   };
 
   const getTrends = (data = [], layout = 'horizontal') => {
-    const trends = data.map((v, i) => <Statistic key={i} {...v} />);
+    const trends = data.map((v: Record<string, any>, i) => <Statistic key={i} {...v} />);
     return layout == 'vertical' ? trends : <Space>{trends}</Space>;
   };
-
-  const getFooter = (config, data) => {
+  const getFooter = (config: Record<string, any>, data: Record<string, any>) => {
     const configFooter = config.footer || { type: 'text' };
     if (!config.open?.footer) {
       //未开启底部设置
@@ -35,37 +39,24 @@ const PanelItemCard = (props) => {
     let footerContent;
     if (configFooter.type == 'trend') {
       //趋势显示
-      if (!data.trend) {
+      if (!data?.trend) {
         //无趋势数据
         return null;
       }
-      footerContent = getTrends(data.trend, configFooter?.layout);
+      footerContent = getTrends(data?.trend, configFooter?.layout);
     } else {
       //文本显示
-      const text = data.footer ? data.footer : configFooter.text;
+      const text = data?.footer ? data.footer : configFooter.text;
       if (!text) {
         return null;
       }
       footerContent = <div dangerouslySetInnerHTML={{ __html: text }}></div>;
     }
-    return (
-      <>
-        <Divider style={{ marginTop: -16, marginBottom: 10 }} />
-        {footerContent}
-      </>
-    );
+    return footerContent;
   };
 
-  const statistic = open?.statistic
-    ? {
-        ...config?.statistic,
-        value: isObj(data) ? data?.value : data,
-        description: getDescription(config, data),
-        title: config?.statistic?.title ? config?.statistic?.title : label,
-      }
-    : false;
   //console.log('PanelItemCard config ', config?.statistic, label);
-  const getChart = (config, data) => {
+  const getChart = (config: Record<string, any>, data: Record<string, any>) => {
     //console.log('getChart ', config, data);
     const { chart = {}, open } = config;
     const { type = '' } = chart;
@@ -78,11 +69,11 @@ const PanelItemCard = (props) => {
     }
     const style = chart?.height ? { height: chart?.height, lineHeight: chart?.height + 'px' } : {};
     if (type == 'trend') {
-      return <div style={style}>{getTrends(data.trend, config.chart?.trend?.layout)}</div>;
+      return <div style={style}>{getTrends(data?.trend, config.chart?.trend?.layout)}</div>;
     } else if (type == 'progress') {
       return (
         <div style={style}>
-          {data.progress?.map((v, i) => (
+          {data?.progress?.map((v: Record<string, any>, i: string) => (
             <Progress key={i} {...v} />
           ))}
         </div>
@@ -90,8 +81,18 @@ const PanelItemCard = (props) => {
     } else {
       return <PanelItemChart {...data?.chart} config={chart} type={type} />;
     }
-    return null;
   };
+  const { title, data = {}, config = {}, height, label } = props;
+  const { open } = config;
+
+  const statistic = open?.statistic
+    ? {
+        ...config?.statistic,
+        value: isObj(data) ? data?.value || '' : data,
+        description: getDescription(config, data),
+        title: config?.statistic?.title ? config?.statistic?.title : label,
+      }
+    : false;
   return (
     <StatisticCard
       footer={getFooter(config, data)}
